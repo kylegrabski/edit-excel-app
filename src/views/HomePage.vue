ecde1161223f0d6b349f278950a96351266dab8d
kylegrabski <kyle.grabski@gmail.com>
2023-03-12 11:03:26 -0700

    bring in org excel object

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,23 +1,34 @@
 <template>
     <div>
-      <h3>
-        UPLOAD AN EXCEL FILE
-      </h3>
-      <div>
-        <v-file-input
-            truncate-length="15"
-        ></v-file-input>
-      </div>
+        <h3>
+            UPLOAD AN EXCEL FILE
+        </h3>
+        <div>
+                <v-file-input v-model="selectedFile" show-size accept="file/" label="File Input">
+                </v-file-input>
+                <v-btn elevation="2" @click="displayExcel()">Display Excel File</v-btn>
+        </div>
     </div>
-  </template>
+</template>
   
-  <script>
-    export default {
-      name: 'HomePage',
-  
-      data: () => ({
-       
-      }),
+<script>
+import OrganizeOriginalExcelFile from "../utilities/organizeOriginalExcelFile"
+
+export default {
+    name: 'HomePage',
+
+    data: () => ({
+        selectedFile: null
+    }),
+    methods : {
+        async displayExcel() {
+            // Display file here
+            // We can then have a button to store the file
+            let orgFile = new OrganizeOriginalExcelFile(this.selectedFile);
+            let ourResults = await orgFile.convertExcelToJson();
+            console.log(`ourResults: `, ourResults);
+        }
     }
-  </script>
+}
+</script>
   
