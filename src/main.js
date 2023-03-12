138431ce3d886cc736adff5fe1be330a1b66250b
kylegrabski <kyle.grabski@gmail.com>
2023-03-12 11:35:03 -0700

    use excel editor

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueExcelEditor from 'vue-excel-editor'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
+Vue.use(VueExcelEditor)
 
 new Vue({
   vuetify,
