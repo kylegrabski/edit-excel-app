f0806ff4a60d2db8fcbd5b72d5d556a8e667802a
kylegrabski <kyle.grabski@gmail.com>
2023-03-12 10:14:45 -0700

    adjust dir structure

---
+++ src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h3>
+        UPLOAD AN EXCEL FILE
+      </h3>
+      <div>
+        <v-file-input
+            truncate-length="15"
+        ></v-file-input>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: 'HomePage',
+  
+      data: () => ({
+       
+      }),
+    }
+  </script>
+  
