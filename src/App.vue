528cfcbbf13dd5a8c2ade63d2e53a9af6a277409
kylegrabski <kyle.grabski@gmail.com>
2023-03-12 10:18:17 -0700

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <v-app>
-    <v-main>
-      <HomePage/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <HomePage/>
+  </div>
 </template>
 
 <script>
-import HomePage from './views/HomePage.vue';
+import HomePage from './views/HomePage.vue'
 
 export default {
   name: 'App',
-
   components: {
-    HomePage,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    HomePage
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
